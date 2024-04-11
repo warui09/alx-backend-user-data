0eed55d666a8bf29d7b22144a03c2910e88fda42
Kevin Warui <waruimacharia@gmail.com>
2024-04-11 10:47:28 +0000

    Add filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""task 0 solution"""
+
+import re
+
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+    """Returns a log message obfuscated"""
+    return re.sub( r"({0}=)([^{1}]+)".format("|".join(fields), separator),
+        r"\1{0}".format(redaction), message,)
