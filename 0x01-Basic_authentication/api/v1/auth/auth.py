a3d91fe5e5b6ae35fe09609ca9c9d1631ed251ee
Kevin Warui <waruimacharia@gmail.com>
2024-04-15 11:01:02 +0000

    Implement require_auth in api/v1/auth/auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,18 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Get authorized paths
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        # handle slashes at end
+        if path[-1] != '/':
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """ Get authorization headers
