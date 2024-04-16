99fe9eb4bc0aeb421478cd387e357759927c4f83
Kevin Warui <waruimacharia@gmail.com>
2024-04-16 09:30:40 +0000

    Edit style in api/v1/auth/auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python3
-""" Auth class
-"""
+"""Auth class"""
 
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """ Auth class
-    """
+    """Auth class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Get authorized paths
-        """
+        """Get authorized paths"""
         if path is None:
             return True
 
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        
+
         # handle slashes at end
-        if path[-1] != '/':
-            path = path + '/'
+        if path[-1] != "/":
+            path = path + "/"
 
         if path in excluded_paths:
             return False
 
     def authorization_header(self, request=None) -> str:
-        """ Get authorization headers
-        """
+        """Get authorization headers"""
         if request is None:
             return None
 
         authorization_header = request.headers.get("Authorization")
         return authorization_header
 
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ Get current user
-        """
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Get current user"""
         return None
