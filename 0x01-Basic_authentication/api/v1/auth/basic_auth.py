06f22bbae0a9bde793527c1928938115eb394d4b
Kevin Warui <waruimacharia@gmail.com>
2024-04-16 15:45:00 +0000

    Add decode_base64_authorization_header to api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+        ) -> str:
+        """Decode value of a Base64 string base64_authorization_header"""
+
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str
+            ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except binascii.Error:
+            return None
