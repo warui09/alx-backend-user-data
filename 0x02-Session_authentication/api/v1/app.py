2fb6ccac5b50671766ecf1e38a252067a55cba21
Kevin Warui <waruimacharia@gmail.com>
2024-04-18 20:59:03 +0000

    Revert last commit

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,7 +11,6 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
