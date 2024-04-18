b5d467b74f0cc4086731eb2b65393fe8e2eff5d0
Kevin Warui <waruimacharia@gmail.com>
2024-04-18 19:13:32 +0000

    Edit api/v1/app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -28,6 +29,9 @@ if auth_type == "basic_auth":
 
 if auth_type == "session_auth":
     auth = SessionAuth()
+
+if auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(404)
