bee7fbc3d9dd91a37d0673f44920aea5f1439076
Kevin Warui <waruimacharia@gmail.com>
2024-04-18 13:44:56 +0000

    Add check for request being None

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -38,5 +38,9 @@ class Auth:
 
     def session_cookie(self, request=None):
         """Returns a cookie value from a request"""
+
+        if request is None:
+            return None
+
         cookie_name = os.getenv("SESSION_NAME")
         return request.cookies.get(cookie_name, None)
