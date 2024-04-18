fcab709187c32f1cc073591c4ebd447ef4992e20
Kevin Warui <waruimacharia@gmail.com>
2024-04-18 14:43:15 +0000

    Add method current_user to api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -24,3 +25,10 @@ class SessionAuth(Auth):
         """Returns a User ID based on a Session ID"""
 
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on a cookie value"""
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
