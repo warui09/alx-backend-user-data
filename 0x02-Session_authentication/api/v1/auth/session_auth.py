8da9e5c44809ae6570fac3b569412f0ef874c1b6
Kevin Warui <waruimacharia@gmail.com>
2024-04-18 13:22:19 +0000

    Add instance method create_session to api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,20 @@
 """Defines class SessionAuth that inherits from Auth"""
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
+
 
 class SessionAuth(Auth):
     """SessionAuth class"""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Ceates a Session ID for a user_id"""
+
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
