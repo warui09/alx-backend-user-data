61984784eff038191f5c7949267bfde33ae3f849
Kevin Warui <waruimacharia@gmail.com>
2024-04-18 15:43:27 +0000

    Refactor api/v1/views/session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -13,22 +13,22 @@ def login() -> str:
     email = request.form.get("email")
     passwd = request.form.get("password")
 
-    if email is None:
+    if email is None or len(email) == 0:
         return jsonify({"error": "email missing"}), 400
 
-    if passwd is None:
+    if passwd is None or len(passwd) == 0:
         return jsonify({"error": "password missing"}), 400
 
-    user = User.search(email)
-    if user is None:
+    user = User.search({"email": email})
+    if user is None or len(user) <= 0:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if user.is_valid_password(passwd):
+    if user[0].is_valid_password(passwd):
         from api.v1.app import auth
 
-        sessiond_id = auth.create_session(getattr(users, "id"))
-        response = jsonify(user.to_json())
-        response.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
+        session_id = auth.create_session(getattr(users[0], "id"))
+        response = jsonify(user[0].to_json())
+        response.set_cookie(os.getenv("SESSION_NAME"), session_id)
         return response
     else:
         return jsonify({"error": "wrong password"}), 401
