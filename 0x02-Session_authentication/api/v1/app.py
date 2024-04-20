aaf6501edf2cd8a44235d5539ba111acba30e47c
Kevin Warui <waruimacharia@gmail.com>
2024-04-20 08:39:45 +0000

    Add session_exp_auth.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
