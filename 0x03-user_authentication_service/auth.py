ee5facb5abbdbc1c9e9a7e774a367f7d2ec97537
Kevin Warui <waruimacharia@gmail.com>
2024-04-23 10:34:01 +0000

    Add auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 """Auth file"""
 
+import bcrypt
 
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password string"""
+
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
