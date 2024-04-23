483361102be01fa954a79833bc30fabd794b88cf
Kevin Warui <waruimacharia@gmail.com>
2024-04-23 08:02:17 +0000

    Fix bug in user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """User model"""
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
