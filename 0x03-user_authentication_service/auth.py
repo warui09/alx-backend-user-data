443c87ed91cc3b66c61c0cdda422e1d59930b379
Kevin Warui <waruimacharia@gmail.com>
2024-04-23 11:11:53 +0000

    Refactor Auth.register_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,9 +12,9 @@ def _hash_password(password: str) -> bytes:
 
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
+
 class Auth:
-    """Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
         self._db = DB()
@@ -24,11 +24,8 @@ class Auth:
 
         # chech if user exists
         try:
-            user = self._db.find_user_by(email=email)
+            self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError("User <user's email> already exists")
+            self._db.add_user(email, _hash_password(password))
 
-        # save user
-        if not user:
-            hash_password = _hash_password(password)
-            return self._db.add_user(email, hash_password)
+        raise ValueError(f"User {email} already exists")
