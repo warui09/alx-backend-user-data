ba28241cb6c95996ad07a841a30b1855d21eb9a0
Kevin Warui <waruimacharia@gmail.com>
2024-04-23 08:12:55 +0000

    Fix bug in DB.add_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,7 +35,7 @@ class DB:
 
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
-        self._session.flush()
+        self._session.commit()
         return new_user
 
     def find_user_by(self, **kwargs: str) -> User:
