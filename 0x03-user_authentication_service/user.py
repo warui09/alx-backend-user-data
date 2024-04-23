2c71ee5d4c0068a2e7e0c431048de51f5951e116
Kevin Warui <waruimacharia@gmail.com>
2024-04-23 08:08:15 +0000

    Fix bug in user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,8 +11,9 @@ class User(Base):
     """user class"""
 
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=True)
-    reset_token = Column(String(255), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
