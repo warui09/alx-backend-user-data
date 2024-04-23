edc1a9111535e6c18203ec1e12752022365ba09b
Kevin Warui <waruimacharia@gmail.com>
2024-04-23 20:04:28 +0000

    Edit logout route definition

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,11 +51,10 @@ def logout() -> str:
 
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect("/")
-
-    abort(403)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
