d8a5c75224dd958ac7e06cd2ccd4ba44b2bdc3f4
Kevin Warui <waruimacharia@gmail.com>
2024-04-23 14:06:10 +0000

    Add register_user method to app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 """Basic Flask App"""
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def root() -> str:
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify('f{"email": {email}, "message": "user created"}')
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
