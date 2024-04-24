4928c83f8b181ac349b33c1670c59e47dd5578ec
Kevin Warui <waruimacharia@gmail.com>
2024-04-24 15:48:52 +0000

    Edit Auth.get_reset_password_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,7 +80,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
-            user["reset_token"] = reset_token
+            self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
             raise ValueError
