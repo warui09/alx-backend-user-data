9547579b71a4da4e80530471c821b1c8617eb9c9
Kevin Warui <waruimacharia@gmail.com>
2024-04-24 16:55:04 +0000

    Fix bug in Auth.update_password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,6 +90,10 @@ class Auth:
 
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            self._db.update_user(user.id, hashed_password=_hash_password(password))
+            self._db.update_user(
+                user.id,
+                hashed_password=_hash_password(password),
+                reset_token=None
+            )
         except NoResultFound:
             raise ValueError
