60762f1ea830e6dc6a5fb69329c91fe3c65de2bb
Kevin Warui <waruimacharia@gmail.com>
2024-04-25 12:03:05 +0000

    Reformat app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,6 +80,7 @@ def get_reset_password_token() -> str:
     except ValueError:
         abort(403)
 
+
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """Update user password"""
@@ -91,7 +92,7 @@ def update_password() -> str:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
-        abort (403)
+        abort(403)
 
 
 if __name__ == "__main__":
